416fe6ec3015c3b25c7ae1c0842e2720b97bd426
Brett Swift <Brett.Swift@sjrb.ca>
2017-02-14 08:08:09 -0700

    Last commit before merging templates
    Unify formation template
    Add service eb service role - still broken
    Tweak route and fix template
    Meaningless Update to test CD Pipeline

---
+++ README.md
@@ -0,0 +1,11 @@
+## elastic beanstalk demo app
+
+this repo is a work in progress. 
+
+## Tenets
+* deployed with Cloud Formation into an existing VPC.
+    * Leveraging existing cross stack references for networks.
+* sets up codepipeline.
+* supports offline development in a Vagrant box.
+    * with local DynamoDB
+
--- routes.rb
+++ routes.rb
@@ -6,7 +6,12 @@ get '/' do
 <<-TERMINATOR
   <h1>Welcome to beanstalk. <br><br></h1>
 
-  You hit host: #{hostname}!
+  <h3>You hit host: #{hostname}!</h3>
+
+<i>
+  This page is pretty useless in dev. But in prod it will provide evidence that the loadbalancer
+  is functioning by showing you the IP of the webserver host.
+</i>
 TERMINATOR
 end
 
